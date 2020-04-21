c4665d028c0382ff35c026dc0487c112731ece3f
Arjun M <arjunmcbe@gmail.com>
2020-04-21 11:39:30 +0530

    ok

--- main.py
+++ main.py
@@ -65,35 +65,59 @@ class Firewall:
             return 
         proto = self.getProtocolProperties(packet,self.protocol)
         self.port,self.dport = self.getPort(proto)
-        return
+        
+
+j = 0
 
 def handlePacket(packet):
     
     firewall = Firewall()
     firewall.checkPacket(packet)
-    
+
+    global j
+
     k = 0
+
+    if firewall.src_ip == socket.gethostbyname(socket.gethostname()):
+        return
+
     for i in rules:
-        if i['action'] == 'block':
-            if firewall.protocol == i['protocol']:
-                if firewall.src_ip == i['srcip'] and firewall.dst_ip == i['dstip']:
-                    if firewall.port == i['hostport'] and firewall.dport == i['otherport']:
-                        string = 'netsh advfirewall firewall add rule name=\"block ' + str(j) + '\"'
-                        if firewall.protocol != 'None':
-                            string += ' protocol=' + firewall.protocol
-                        if firewall.src_ip != 'None':
-                            string += ' remoteip=' + firewall.src_ip
-                        if firewall.dst_ip != 'None':
-                            string += ' localip=' + firewall.dst_ip
-                        if firewall.port != -1:
-                            string += ' localport=' + firewall.port
-                        if firewall.dport != -1:
-                            string += ' remoteport=' + firewall.dport
-                        if k == 0 and string != 'netsh advfirewall firewall add rule name=\"block ' + str(j) + '\"':
-                            subprocess.getoutput(string)
-                            j = j + 1
-                            count[k] = -1
+        
+        print(firewall.protocol == i['protocol'])
+
+        if (firewall.protocol == i['protocol'] and firewall.protocol != 'None') \
+        or (firewall.src_ip == i['srcip'] and firewall.src_ip != 'None')\
+        or (firewall.dst_ip == i['dstip'] and firewall.dst_ip != 'None'):
+            
+            print('Packet with ip',firewall.src_ip,'is blocked')
+            
+            string = 'netsh advfirewall firewall add rule name=\"block ' + str(j) + '\" dir=in'
+            
+            if firewall.protocol == i['protocol'] and firewall.protocol != 'None':
+                string += ' protocol=' + firewall.protocol
+            
+            if firewall.src_ip == i['srcip'] and firewall.src_ip != 'None':
+                string += ' remoteip=' + firewall.src_ip
+            
+            if firewall.dst_ip == i['dstip'] and firewall.dst_ip != 'None':
+                string += ' localip=' + firewall.dst_ip
+            
+            if firewall.port == i['hostport'] and i['hostport'] != -1:
+                string += ' localport=' + firewall.port
+            
+            if firewall.dport == i['otherport'] and i['otherport'] != -1:
+                string += ' remoteport=' + firewall.dport
+            
+            string += ' action=block'
+            
+            if count[k] == 0 and string != 'netsh advfirewall firewall add rule name=\"block ' + str(j) + '\"':
+                subprocess.getoutput(string)
+                print(string)
+                print('Ok.')
+                j = j + 1            
+                count[k] = -1
         k = k + 1
+        
 
                         
                 
@@ -109,7 +133,7 @@ def checkIP(ip):
 
 if __name__ == '__main__':
 
-    global rules,count,j
+    global rules,count
 
     elevate()
 
@@ -117,13 +141,10 @@ if __name__ == '__main__':
 
     rules = []
     count = []
-    j = 0
 
     for i in range(val):
         d = {}
         
-        d['action'] = input('Rule' + str(i+1) + ' (Allow/Block): ')
-        d['action'] = d['action'].lower()
         d['protocol'] = input('Protocol(TCP,UDP,ICMP,None):')
         
         d['srcip'] = input('Source IP(IP,None):')
@@ -158,4 +179,5 @@ if __name__ == '__main__':
     
     capture.apply_on_packets(handlePacket,packet_count = 25)
 
+    print('Done...')
     input()
